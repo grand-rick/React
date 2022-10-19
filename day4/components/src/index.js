a6a4882d5a1b8bf7126161e97bae175442462fce
Patrick Kabuga <kabugap02@gmail.com>
2022-10-19 23:01:15 +0300

    Done with exercise1 and 2 of day4

---
+++ day4/components/src/index.js
@@ -0,0 +1,67 @@
+// index.js
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import asabenehImage from 'https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__340.jpg'
+
+// Header Component
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Asabeneh Yetayeh</p>
+      <small>Oct 3, 2020</small>
+    </div>
+  </header>
+)
+
+// User Card Component
+const UserCard = () => (
+  <div className='user-card'>
+    {/* <img src={asabenehImage} alt='asabeneh' /> */}
+    <h2>Asabeneh Yetayeh</h2>
+  </div>
+)
+
+// TechList Component
+const TechList = () => {
+  const techs = ['HTML', 'CSS', 'JavaScript']
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+  return techsFormatted
+}
+
+// Main Component
+const Main = () => (
+  <main>
+    <div className='main-wrapper'>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList />
+      </ul>
+      <UserCard />
+    </div>
+  </main>
+)
+
+// Footer Component
+const Footer = () => (
+  <footer>
+    <div className='footer-wrapper'>
+      <p>Copyright 2020</p>
+    </div>
+  </footer>
+)
+
+// The App, or the parent or the container component
+const App = () => (
+  <div className='app'>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
+
+const rootElement = document.getElementById('root');
+// we render the App component using the ReactDOM package
+ReactDOM.render(<App />, rootElement)
